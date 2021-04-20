0d73d56c2e78087384964b1fdbc17304ab635c1c
谌丹杨 <karliechen@chendanyangdeMacBook-Pro.local>
2021-04-20 01:36:47 -0700

    add .gitignore

--- w2-2.R
+++ w2-2.R
@@ -4,3 +4,18 @@ lightHour <- lightMinut*60
 lightDay <- lightHour*24
 lightYear <- lightDay*365.2422
 cat("lenghth of light year:", lightYear, "\n")
+add <- function(x,y){
+  z <- x + y
+  cat("x=", x, "y=", y, "\n")
+  return(z)
+}
+
+
+lightyear <- function(ly){
+  c <- 299792458
+  lightMinut <- c*60
+  lightHour <- lightMinut*60
+  lightDay <- lightHour*24
+  lightYear <- lightDay*365.2422
+  lightYear*ly
+}
